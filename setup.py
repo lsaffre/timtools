5dcb8501a0f1be9299cc821a25ebf2df59430ef8
Luc Saffre <luc.saffre@gmail.com>
2019-09-13 13:03:04 +0300

    http://luc.lino-framework.org/blog/2019/0913.html

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-import sys
-if sys.version_info[0] != 2:
-    #raise Exception("Requires Python 2")
-    print("Requires Python 2. Exiting silently.")
-    sys.exit()
-
-from setuptools import setup
 fn = 'timtools/setup_info.py'
 exec(compile(open(fn, "rb").read(), fn, 'exec'))
 
+from setuptools import setup
+import sys
+
 if __name__ == '__main__':
-    setup(**SETUP_INFO)
+
+    if sys.version_info[0] != 2:
+        #raise Exception("Requires Python 2")
+        print("Requires Python 2. Exiting silently.")
+    else:
+        setup(**SETUP_INFO)
